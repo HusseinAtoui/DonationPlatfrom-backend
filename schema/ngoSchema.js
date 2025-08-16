c70ab6971e6e79d5497681074bb5058cc439334e
hms65 <hms65@mail.aub.edu>
2025-08-16 16:38:53 +0300

    Updated NGO schema with location for map

--- schema/ngoSchema.js
+++ schema/ngoSchema.js
@@ -4,7 +4,13 @@ const ngoSchema = z.object({
   email: z.string().email(),
   phone: z.string().min(6),
   name: z.string().min(1),
-  location: z.string().min(1),
+  location: z.object({
+    address: z.string().min(1),
+    coordinates: z.object({
+      lat: z.number().min(-90).max(90),
+      lng: z.number().min(-180).max(180)
+    })
+  }),
   password: z.string().min(6),
   inventorySize: z.number().nonnegative().optional(),
   requiredClothing: z.string().optional(),
